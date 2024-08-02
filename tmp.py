1fda26706e6ddd9a1d96113144b24e3196c01078
richard.blackwell@lumen.com <rblackwe@nocsupidc1.idc1.level3.com>
2024-08-02 15:10:27 +0000

    added gitignore file and started pid lock function

--- tmp.py
+++ tmp.py
@@ -1,5 +1,19 @@
-import time
-from datetime import datetime
+script_path = "/export/home/rblackwe/scripts/aorc_modify_prefix_script/"
+resource_name = f'{script_path}/lock/__user_file__'
+lockfile_dir_path = f'{script_path}/lock/__lock_file__'
 
-print(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
-print(f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}")
+from resource_lock import LockablePidFile  
+
+lock = LockablePidFile(resource_name, lockfile_dir_path)
+
+token = lock.acquire():
+if token is not None:
+    # the lock is successfully acquired
+    send_file_to_remote_device(.....)
+
+    lock.release(token)
+else:
+    # failed to acquire lock - print an error message
+
+    print(f"Failed to acquire lock details are : {lock.error_msg}")
+
